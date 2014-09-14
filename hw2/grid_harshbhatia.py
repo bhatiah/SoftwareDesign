2467e31d220bade9304eef192522496cb9ee1837
Harsh Bhatia <harshvardhan.bhatia@students.olin.edu>
2014-09-14 16:49:39 -0400

    My try for grid structure.

---
+++ hw2/grid_harshbhatia.py
@@ -0,0 +1,79 @@
+def fourtime(d):
+    print d,
+    print d,
+    print d,
+    print d,
+    
+def across():
+    print '+',    
+    fourtime("-")
+    
+def mid3():
+    print '|',
+    fourtime(' ')
+    print '|',
+    fourtime(' ')
+    print '|'
+
+def mid4():
+    print '|',
+    fourtime(' ')    
+    print '|',
+    fourtime(' ')
+    print '|',
+    fourtime(' ')
+    print '|'
+
+def grid3x3():
+    across()
+    across()
+    print '+'
+    mid3()
+    mid3()
+    mid3()
+    mid3()
+    across()
+    across()
+    print '+'
+    mid3()
+    mid3()
+    mid3()
+    mid3()
+    across()
+    across()
+    print '+'
+    
+def grid4x4():
+    across()
+    across()
+    across()    
+    print '+'
+    mid4()
+    mid4()
+    mid4()
+    mid4()
+    across()
+    across()
+    across()    
+    print '+'
+    mid4()
+    mid4()
+    mid4()
+    mid4()
+    across()
+    across()
+    across()    
+    print '+'
+    mid4()
+    mid4()
+    mid4()
+    mid4()   
+    across()
+    across()
+    across()    
+    print '+'
+       
+grid3x3()
+grid4x4()
+
+
